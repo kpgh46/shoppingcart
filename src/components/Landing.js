e1e5648b962a84a95d8ce5f71227a46032d9f3e5
Kevin McPeak <kevmc46@gmail.com>
2022-05-31 19:50:38 -0400

    added cover photo on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,17 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
+import Cover from "../Assets/coverPhotos/pexels-mark-mccammon-1080696.jpg";
 
 let LandingPage = () => {
 	return (
 		<div>
-			<h1>Landing Page!</h1>
+			<h1>Landing Page</h1>
 			<h3>
 				<Link to="/home">Home</Link>
 			</h3>
+			<div className="landing-container">
+				<img className="main-cover-photo" src={Cover} alt="cover"></img>
+			</div>
 		</div>
 	);
 };
