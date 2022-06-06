93a43dff5e8f2258659e41def90aa6979adcac99
Kevin McPeak <kevmc46@gmail.com>
2022-06-06 19:55:27 -0400

    added overlay to shoppingPage

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,26 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
+import { faL } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Link } from "react-router-dom";
 
 let Item = (props) => {
 	let source = props.source;
-	// console.log(props);
+
+	let [over, setOver] = React.useState(false);
+	console.log(over);
 
 	return (
-		<div>
+		<div
+			onMouseOver={() => setOver(true)}
+			onMouseOut={() => setOver(false)}
+		>
 			<Link to={`itemdetail/${props.id}`}>
 				<img
-					className="main-item-photo"
+					className={
+						over
+							? "main-item-photo item-overlay"
+							: "main-item-photo"
+					}
 					src={source}
 					alt="item photo"
 				></img>
