ad24c09ea3d5ea0052ba2cfd0792892d17db5bdc
Kevin McPeak <kevmc46@gmail.com>
2022-06-07 19:17:25 -0400

    updated CSS on details page

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import data from "../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faTruckArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 let ItemDetail = (props) => {
 	let { id } = useParams();
+	let truck = <FontAwesomeIcon icon={faTruckArrowRight} />;
 
 	let product = data.find((item) => {
 		return item.id === parseInt(id);
 	});
 
-	// let handleChange = (id) => {
-	// 	props.onClick(id);
-	// };
+	let capitalize = (string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	};
 
 	return (
 		<div className="itemdetail-container">
@@ -24,12 +28,18 @@ let ItemDetail = (props) => {
 			</div>
 
 			<div className="itemdetail-right">
-				<h2>{product.title}</h2>
-				<h4>{product.price}</h4>
+				<h2>{capitalize(product.title)}</h2>
+				<h4>${product.price}</h4>
 				<p>{product.description}</p>
-				<button onClick={() => props.handleAddToCart(parseInt(id))}>
-					Add to Cart
+				<button
+					className="addToCart"
+					onClick={() => props.handleAddToCart(parseInt(id))}
+				>
+					Add to Cart - ${product.price}
 				</button>
+
+				<div className="shipping"></div>
+				{truck}
 			</div>
 		</div>
 	);
